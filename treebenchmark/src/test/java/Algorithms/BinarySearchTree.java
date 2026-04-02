90121f7ab2bea8b5fe860dfaa0c439b531a672e6
Nourm147 <nourm15952@gmail.com>
2026-04-03 01:55:20 +0200

    create basic binary tree implementation

---
+++ treebenchmark/src/test/java/Algorithms/BinarySearchTree.java
@@ -0,0 +1,15 @@
+package Algorithms;
+
+public class BinarySearchTree extends AbstractBinarySearchTree<Integer> {
+
+    @Override
+    public void insert(Integer v) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void delete(Integer v) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
